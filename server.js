471a537b76ba3f7eb0f91ee1110d1c595d8abd35
Rocktim53 <rocktim53@gmail.com>
2017-08-04 01:07:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,6 +95,16 @@ app.get('/counter',function(req, res){
     res.send(counter.toString());
 });
 
+var names=[];
+app.get('/submit-name/:name', function(req, res){   //URL: /submit-name?name=xxxxx
+   //Get the name from the request
+   var name=req.query.name; //TODO
+   
+   names.push(name);
+   //JSON Javascript Object Notation
+   res.send(JSON.stringify(names)); //TODO
+});
+
 app.get('/:articleName',function(req,res){
     // articleName == article=one
     // articles[articleName] == {} content object for article one
@@ -119,15 +129,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var names=[];
-app.get('/submit-name/:name', function(req, res){
-   //Get the name from the request
-   var name=req.params.name; //TODO
-   
-   names.push(name);
-   //JSON Javascript Object Notation
-   res.send(JSON.stringify(names)); //TODO
-});
+
 
 var port = 80;
 app.listen(port, function () {
